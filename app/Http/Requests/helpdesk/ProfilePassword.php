889483a9304f0662bb8d238f4386ec334d0f7369
Muhammad Sukri <epilim88@gmail.com>
2024-12-16 13:39:09 +0800

    enhance password rules on profile update change password

--- app/Http/Requests/helpdesk/ProfilePassword.php
+++ app/Http/Requests/helpdesk/ProfilePassword.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\helpdesk;
 
 use App\Http\Requests\Request;
+use Illuminate\Validation\Rules\Password;
 
 /**
  * ProfilePassword.
@@ -30,8 +31,10 @@ class ProfilePassword extends Request
     {
         return [
             'old_password'     => 'required',
-            'new_password'     => 'required|min:6',
-            'confirm_password' => 'required|same:new_password',
+            // 'new_password'     => 'required|min:6',
+            'new_password'     => ['required', Password::min(12)->letters()->mixedCase()->numbers()->symbols()],
+            // 'confirm_password' => 'required|same:new_password',
+            'confirm_password' => ['required', 'same:new_password', Password::min(12)->letters()->mixedCase()->numbers()->symbols()],
         ];
     }
 }
